0cb134f1015c1dba686e0fb0035d5efea56713ed
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2023-04-05 21:04:40 -0700

    added category selector

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -1,14 +1,30 @@
+import { useDispatch } from "react-redux";
+import productSlice from "../../store/product";
+
 const Categories = () => {
+  const dispatch = useDispatch();
   return (
-    <section     style={{
+    <section
+      style={{
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        gap: "10px"
-      }}>
+        gap: "10px",
+      }}
+    >
       <h3>Browse our Categories</h3>
-      <select>
+      <select
+        onChange={(e) => {
+          const category = e.target.value;
+          dispatch(
+            productSlice.actions.setActiveCategory(
+              category === "" ? undefined : category
+            )
+          );
+        }}
+      >
+        <option value="">All</option>
         <option value="electronics">Electornics</option>
         <option value="food">Food</option>
         <option value="clothing">Clothing</option>
--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -1,11 +1,14 @@
 import "./products.css";
 import Product from "../Product";
+import { filteredProducts } from "../../store/product";
 import { useSelector } from "react-redux";
 
 // const data = require("../../data.json");
 
 const Products = () => {
-  const products = useSelector((state) => state.product.products);
+  // const products = useSelector((state) => state.product.products);
+    const products = useSelector(filteredProducts);
+
   return (
     <section>
       {products.map((product) => (
